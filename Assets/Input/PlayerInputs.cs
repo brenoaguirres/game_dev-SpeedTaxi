b216ae477d63fd5142ba3831ca3372e297ea2514
brenoaguirres <bennyaguirres1998@gmail.com>
2024-11-22 00:16:04 -0300

    PSX GFX and Custom Camera

---
+++ Assets/Input/PlayerInputs.cs
@@ -0,0 +1,66 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace SpeedTaxi.Inputs
+{
+    public class PlayerInputs : MonoBehaviour
+    {
+        #region FIELDS
+        
+        private PlayerInput _playerInput;
+        private PlayerInputActions _inputActions;
+        
+        private float _accelerate;
+        private float _steer;
+        private bool _handbrake;
+        private bool _skill;
+
+        #endregion
+
+        #region PROPERTIES
+
+        public float Accelerate => _accelerate;
+        public float Steer => _steer;
+        public bool Handbrake => _handbrake;
+        public bool Skill => _skill;
+        
+        #endregion
+        
+        #region UNITY CALLBACKS
+
+        private void Awake()
+        {
+            _inputActions = new PlayerInputActions();
+            
+            _inputActions.Enable();
+            
+            _inputActions.Car.Accelerate.started += ctx => _accelerate = ctx.ReadValue<float>();
+            _inputActions.Car.Steer.started += ctx => _steer = ctx.ReadValue<float>();
+            _inputActions.Car.Handbrake.started += ctx => _handbrake = ctx.ReadValueAsButton();
+            _inputActions.Car.Skill.started += ctx => _skill = ctx.ReadValueAsButton();
+            
+            _inputActions.Car.Accelerate.canceled += ctx => _accelerate = ctx.ReadValue<float>();
+            _inputActions.Car.Steer.canceled += ctx => _steer = ctx.ReadValue<float>();
+            _inputActions.Car.Handbrake.canceled += ctx => _handbrake = ctx.ReadValueAsButton();
+            _inputActions.Car.Skill.canceled += ctx => _skill = ctx.ReadValueAsButton();
+        }
+
+        private void OnEnable()
+        {
+            _inputActions.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _inputActions.Disable();
+        }
+
+        private void OnDestroy()
+        {
+            _inputActions.Disable();
+        }
+
+        #endregion
+    }
+}
--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -1,19 +1,88 @@
+using System;
 using UnityEngine;
+using SpeedTaxi.Inputs;
+using UnityEngine.Serialization;
 
 namespace SpeedTaxi.Camera
 {
     public class CameraFollow : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
+        #region FIELDS
+        [Header("Follow Settings")]
+        [Tooltip("The object which the camera should follow.")]
+        [SerializeField] private Transform _followObject;
+        private Vector3 _initialOffset;
+        
+        [Tooltip("The offset to wait before following.")] 
+        [SerializeField] private Vector3 _followOffset = new Vector3(0, 4f, 2.5f);
+        private Vector3 _currentFollowOffset;
+        
+        [Space(2)]
+        [Header("Input")]
+        [Tooltip("Player Inputs from which the camera observes if is there any movement to offset.")]
+        [SerializeField] private PlayerInputs _input;
 
+        private float _lerpFactor = 0f;
+        private float _lastInput = 0f;
+        [Tooltip("Speed in which the camera lerps to offset when moving.")]
+        [SerializeField] private float _cameraSpeed = 5f;
+        #endregion
+        
+        #region PROPERTIES
+
+        private float LastInput
+        {
+            set
+            {
+                if (_lastInput == value) return;
+                
+                _lastInput = value;
+                ResetOffsetLerp();
+            }
+        }
+        
+        #endregion
+        
+        #region UNITY CALLBACKS
+
+        private void Awake()
+        {
+            _initialOffset = transform.position - _followObject.position;
+            _currentFollowOffset = new Vector3(0, 0, 0);
         }
 
-        // Update is called once per frame
-        void Update()
+        private void Update()
         {
-
+            _lerpFactor += Mathf.Clamp01(Time.deltaTime / _cameraSpeed);
+            
+            if (_input.Accelerate > 0.05f)
+            {
+                LastInput = 1;
+                _currentFollowOffset = Vector3.Lerp(_currentFollowOffset, _followOffset, _lerpFactor);
+            }
+            else if (_input.Accelerate < -0.05f)
+            {
+                LastInput = -1;
+                _currentFollowOffset = Vector3.Lerp(_currentFollowOffset, 
+                    new Vector3(_followOffset.x, _followOffset.y, -_followOffset.z * 2f), _lerpFactor);
+            }
+            else
+            {
+                LastInput = 0;
+                _currentFollowOffset = Vector3.Lerp(_currentFollowOffset, Vector3.zero, _lerpFactor);
+            }
+            
+            transform.position = transform.position = _followObject.position + _initialOffset + _currentFollowOffset;
         }
+        #endregion
+        
+        #region CUSTOM METHODS
+
+        private void ResetOffsetLerp()
+        {
+            _lerpFactor = 0;
+            
+        }
+        #endregion
     }
 }
